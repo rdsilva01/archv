b3ccbaa2163ce2402a893a3129106bb336fc421e
rodrigo <rodriddsilva@gmail.com>
2024-10-24 14:18:48 +0100

    added a function that retrieves the tts to a json of docs

--- archv/__init__.py
+++ archv/__init__.py
@@ -10,7 +10,7 @@ __email__ = 'rd.silva@ubi.pt'
 
 # News retrieval functionalities
 from .news_retrieval.fetch_news_articles import get_past_urls, get_news_articles_urls, get_news_articles_data
-from .news_retrieval.process_news_articles import extract_keywords, extract_keywords_news_articles, extract_named_entities, extract_named_entities_news_articles, get_embeddings_bert, get_news_articles_embeddings, get_text_to_speech
+from .news_retrieval.process_news_articles import extract_keywords, extract_keywords_news_articles, extract_named_entities, extract_named_entities_news_articles, get_embeddings_bert, get_news_articles_embeddings, get_text_to_speech,get_news_articles_tts
 
 # Recommendation system functionalities
 from .recommendation_system.recommend import get_news_articles_similarity
@@ -26,5 +26,5 @@ __all__ = [
     'extract_named_entities_news_articles', 'get_embeddings_bert', 'get_news_articles_embeddings',
     'get_news_articles_similarity', 'connect_redis', 'drop_data', 'drop_index',
     'create_index', 'index_documents', 'populate', 'verify', 'populate_db', 'populate_embeddings', 
-    'get_text_to_speech'
+    'get_text_to_speech', 'get_news_articles_tts'
 ]
